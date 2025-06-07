0b9a68a470a0096c3284faf9721d7195489beabd
Beesy23 <Nathan.Beese@gmail.com>
2025-06-07 10:04:07 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+model_name = "gemini-2.0-flash-001"
+
+def main ():
+    load_dotenv()
+    args = sys.argv[1:]
+    if not args :
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    prompt = args[0]
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model = model_name, 
+        contents = messages,
+    )
+    print(response.text)
+    print("Prompt tokens: {}".format(response.usage_metadata.prompt_token_count) )
+    print("Response tokens: {}".format(response.usage_metadata.candidates_token_count) )
+
+if __name__ == "__main__":
+    main()
