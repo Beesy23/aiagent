9448d3a4e4901ac2b67baca945640bcda1bc3751
Beesy23 <Nathan.Beese@gmail.com>
2025-06-08 12:52:57 +0100

    Prep for optional arguements

--- main.py
+++ main.py
@@ -15,21 +15,23 @@ def main ():
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    prompt = args[0]
+    user_prompt = " ".join(args)
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     messages = [
-        types.Content(role="user", parts=[types.Part(text=prompt)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
     response = client.models.generate_content(
         model = model_name, 
         contents = messages,
     )
+    
+    print("User prompt: {}".format(user_prompt))
+    print("Prompt tokens: {}".format(response.usage_metadata.prompt_token_count))
+    print("Response tokens: {}".format(response.usage_metadata.candidates_token_count))
     print(response.text)
-    print("Prompt tokens: {}".format(response.usage_metadata.prompt_token_count) )
-    print("Response tokens: {}".format(response.usage_metadata.candidates_token_count) )
 
 if __name__ == "__main__":
     main()
