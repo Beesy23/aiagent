f8e12d3e4ecf8b067300c267365fd769b688871c
Beesy23 <Nathan.Beese@gmail.com>
2025-06-12 09:32:23 +0100

    add get_file_content function and tests for it

--- tests.py
+++ tests.py
@@ -2,9 +2,26 @@
 
 import unittest
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 class TestAgent(unittest.TestCase):
-    def test_get_file_info_current_dir(self):
+    def test_get_file_contents_main(self):
+        print("Result for main.py:")
+        print(f'{get_file_content("calculator", "main.py")}\n')
+
+    def test_get_file_contents_calculator(self):
+        print("Result for pkg/calculator.py:")
+        print(f'{get_file_content("calculator", "pkg/calculator.py")}\n')
+
+    def test_get_file_contents_cat(self):
+        print("Result for /bin/cat:")
+        print(f'{get_file_content("calculator", "/bin/cat")}\n')
+
+    """def test_get_file_contents_lorem(self):
+        print("Result for lorem.txt:")
+        print(f'{get_file_content("calculator", "lorem.txt")}\n')
+
+     def test_get_file_info_current_dir(self):
         print("Result for current directory:")
         print(f'{get_files_info("calculator",".")}\n')
 
@@ -18,7 +35,8 @@ class TestAgent(unittest.TestCase):
 
     def test_get_file_info_self(self):
         print("Result for '../' directory:")
-        print(f'{get_files_info("calculator","../")}\n')
+        print(f'{get_files_info("calculator","../")}\n') """
+
 
 if __name__ == "__main__":
     unittest.main()
